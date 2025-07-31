a96ed9cf1b6c8598fab56d993ea7be82d742c32c
19960620vV <vlados117@gmail.com>
2025-07-31 03:20:02 +0300

    add scss global variables and styles for public layout header. Start public layout header component

--- components/layout/LayoutHeader.vue
+++ components/layout/LayoutHeader.vue
@@ -2,8 +2,18 @@
 </script>
 
 <template>
-111111111
+  <div class="header">
+    <div class="logo">Family Recipe Book</div>
+    <div class="nav-icons">
+      <img src="" alt="Меню" class="sidebar-toggle" onclick="toggleSidebar()" />
+      <img src="" alt="Поиск" />
+      <img src="" alt="Уведомления" />
+      <img src="" alt="Профиль" />
+      <img src="" alt="Выход" />
+    </div>
+  </div>
 </template>
 
 <style lang="scss">
+
 </style>
