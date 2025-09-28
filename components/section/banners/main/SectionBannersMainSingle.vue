89bd2207e0275611f837dfc382600e30d7af92b7
root <root@mweuyquaqx.local>
2025-09-28 00:09:49 +0000

    sync

--- components/section/banners/main/SectionBannersMainSingle.vue
+++ components/section/banners/main/SectionBannersMainSingle.vue
@@ -2,34 +2,37 @@
 const config = useRuntimeConfig()
 const bannersStore = useBanners().banners
 const bannerCode = 'daily_dish'
-const item =  bannersStore[bannerCode][0]
+const bannerItems = bannersStore[bannerCode]
+const item = bannerItems && bannerItems.length > 0 ? bannerItems[0] : null
 
 </script>
 
 <template>
+  <div v-if="item">
+    <a
+        :href="item.url"
+        target="_blank"
+        rel="noopener noreferrer"
+        class="banner"
+    >
+      <div class="banner">
+        <h2 class="banner-title">{{ item.title }}</h2>
 
-    <div>
-      <a
-          :href="item.url"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="banner"
-      >
-        <div
-            class="banner"
-        >
-          <h2 class="banner-title">{{ item.title }}</h2>
+        <img
+            :src="config.public.filesUrl + item.image_path"
+            :alt="item.title"
+            class="banner-image"
+        />
 
-          <img
-              :src="config.public.filesUrl + item.image_path"
-              class="banner-image"
-              alt="Banner"
-          />
+        <p class="banner-description">{{ item.short_description }}</p>
+      </div>
+    </a>
+  </div>
 
-          <p class="banner-description">{{ item.short_description }}</p>
-        </div>
-      </a>
-    </div>
+  <!-- Fallback если данных нет -->
+  <div v-else class="banner-placeholder">
+    <p>Баннер временно недоступен</p>
+  </div>
 </template>
 
 <style scoped>
